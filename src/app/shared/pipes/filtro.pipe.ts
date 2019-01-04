d026924c72f1c752f0e22cbf03e17994080b3e0f
mvdcreativo <mvdcreativo@gmail.com>
2019-01-04 01:45:48 -0300

    cambios en vista detalle

--- src/app/shared/pipes/filtro.pipe.ts
+++ src/app/shared/pipes/filtro.pipe.ts
@@ -6,22 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FiltroPipe implements PipeTransform {
   resultado: any = null;
 
-  transform(value: any, campo: any, criterio?: any): any {
+  transform(value: any , campo: any, criterio?: any): any {
+    if (!criterio) return value;
 
-    if(!criterio){
-      this.resultado = value.filter(
-        val =>{
-          return val[campo] !== null;
-        });
-    }
+    // if(!criterio){
+    //   this.resultado = value.filter(
+    //     val =>{
+    //       return val[campo] !== null;
+    //     });
+    // }
 
     if(criterio){
       if(criterio === 'none'){
 
-        this.resultado = value.filter(
-          val => {
-            return val[campo] == null
-          });
+
 
       }else{
 
